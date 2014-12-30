10e7ffb395fb5b4b6ae8972ea6fa5d2d9422c349
Stuart Boston <Stuart.Boston@Gmail.com>
2014-12-30 19:47:34 +0000

    Update copyright date

--- src/main/java/com/moviejukebox/allocine/model/Channel.java
+++ src/main/java/com/moviejukebox/allocine/model/Channel.java
@@ -1,4 +1,25 @@
-
+/*
+ *      Copyright (c) 2004-2015 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      This file is part of the Yet Another Movie Jukebox (YAMJ).
+ *
+ *      The YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ */
 package com.moviejukebox.allocine.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -6,9 +27,9 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("channel")
 public class Channel extends AbstractJsonMapping {
-    
+
     private static final long serialVersionUID = -5954100778647649863L;
-    
+
     @JsonProperty("name")
     private String name;
 
